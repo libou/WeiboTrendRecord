03c39875e1e3191f0165ffeb7736ec524a25170d
libou <libou0924@163.com>
2020-03-15 21:12:47 -0700

    fix bug

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ from time import sleep
 def main():
     sched_time = [9, 16, 22]
     i = 0
+    current_hour = datetime.now().hour
+    for e in sched_time:
+        if current_hour > e:
+            i = (i + 1) % 3
 
     try:
         while True:
@@ -22,10 +26,10 @@ def main():
                 i = (i+1) % len(sched_time)
                 sleep(61 * 60)
             else:
-                if now.hour - sched_time[i] > 1:
-                    sleep((now.hour - sched_time[i] - 1) * 60 * 60)
-                else:
-                    sleep(1)
+                # if sched_time[i] - now.hour > 1:
+                #     sleep((now.hour - sched_time[i] - 1) * 60 * 60)
+                # else:
+                sleep(1)
     except Exception as e:
         logging.error(e)
         mailObj.notification(e)
